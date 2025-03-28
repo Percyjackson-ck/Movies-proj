28840394898ddec34a67bb23312dedfac0c40215
Percyjackson-ck <girishck04@gmail.com>
2025-03-28 12:49:05 +0530

    day5

--- frontend/src/redux/feature/auth/authSlice.js
+++ frontend/src/redux/feature/auth/authSlice.js
@@ -8,4 +8,20 @@ const initialState={
 const authSlice=createSlice({
     name:"auth",
     initialState,
+    reducers:{
+        setCredentails:(state,action)=>{
+            state.userInfo=action.payload
+            localStorage.setItem('userInfo',JSON.stringify(action.payload))
+
+            const expirationTime=new Date().getTime()+30*24*60*60*1000;
+            localStorage.setItem('expirationTime',expirationTime)
+        },
+        logout:(state)=>{
+            state.userInfo=null;
+            localStorage.clear();
+        },
+    },
 })
+
+export const{setCredentails,logout}=authSlice.actions
+export default authSlice.reducer;
